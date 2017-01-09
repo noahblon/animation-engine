3119fc239a1e1815156b8a7317b029a36983f872
Noah Blon <noahblon@gmail.com>
2017-01-09 08:05:30 -0600

    chore(lib): add unit testing packages and scripts
    * add nyc for code coverage reporting
    * add blue-tape and tap-diff for unit testing
    * add cross-env for eventually running unit tests on transpired source

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+const test = require('blue-tape')
+const sample = require('../src/index')
+
+test('should pass', assert => {
+    assert.equal(
+        sample.test(),
+        true,
+        'equals true!'
+    )
+    assert.end()
+})
